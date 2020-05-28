2e83f793396dcb7dcd38d09664fca47e7e04e233
Barnabe Dubus <barnabe.dubus@gmail.com>
2020-05-28 20:06:25 +0200

    wip controllers

--- app/controllers/pieces_controller.rb
+++ app/controllers/pieces_controller.rb
@@ -1,22 +1,46 @@
 class PiecesController < ApplicationController
+  before_action :set_piece, only: [:show, :edit, :update, :destroy]
+
   def create
+    @piece = Piece.new(piece_params)
+    if @piece.save
+      redirect_to piece_path(@piece)
+    else
+      render :new
+    end
   end
 
   def destroy
+    @piece.destroy
+    redirect_to pieces_path
   end
 
   def edit
+    @piece.update(piece_params)
+    redirect_to piece_path(@piece)
   end
 
   def index
+    @pieces = Piece.all
   end
 
   def new
+    @piece = Piece.new
   end
 
   def show
   end
 
   def update
+  end
+
+  private
+
+  def set_piece
+    @piece = Pieces.find(params[:id])
+  end
+
+  def piece_params
+    params.require(:piece).permit(:name, :photo)
   end
 end
