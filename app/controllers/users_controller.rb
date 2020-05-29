6932056cc771e7eaa32d645408e96104ee786c4d
Francois Jouvray <francois0690@gmail.com>
2020-05-29 18:49:07 +0200

    dashboard v0.1

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,6 +2,13 @@ class UsersController < ApplicationController
 
   def dashboard
     @transactions = current_user.transactions.all
+    @total = 0
+    current_user.transactions.all.each do |t|
+       @total += t.piece.price
+       p @total
+    end
+
+    @pieces = Piece.all
     # @pieces = current_user.pieces
   end
 end
