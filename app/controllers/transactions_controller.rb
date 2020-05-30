2fe9979634ef6de9f85eaa7617560f47d7f4b9f8
Myriam de Bossoreille <myriam.dbdr@gmail.com>
2020-05-30 21:59:27 +0200

    use carousel, ajout du mantant de la loc

--- app/controllers/transactions_controller.rb
+++ app/controllers/transactions_controller.rb
@@ -37,6 +37,7 @@ class TransactionsController < ApplicationController
   end
 
   def show
+    @montant = montant(@transaction)
   end
 
   def update
@@ -45,6 +46,11 @@ class TransactionsController < ApplicationController
   end
 
   private
+
+  def montant(transaction)
+    days = transaction.end - transaction.start
+    (days * transaction.piece.price).ceil
+  end
 
   def transaction_params
     params.require(:transaction).permit(:start, :end, :piece_id)
